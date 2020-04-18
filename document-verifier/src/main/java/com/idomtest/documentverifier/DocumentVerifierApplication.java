b142a9929edee52ed0727167d61a3b0f9119a7a5
Kismoha <43534419+Kismoha@users.noreply.github.com>
2020-04-18 19:04:05 +0200

    Initial commit

---
+++ document-verifier/src/main/java/com/idomtest/documentverifier/DocumentVerifierApplication.java
@@ -0,0 +1,13 @@
+package com.idomtest.documentverifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentVerifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentVerifierApplication.class, args);
+	}
+
+}
---
+++ person-verifier/src/main/java/com/idomtest/personverifier/PersonVerifierApplication.java
@@ -0,0 +1,13 @@
+package com.idomtest.personverifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonVerifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonVerifierApplication.class, args);
+	}
+
+}
